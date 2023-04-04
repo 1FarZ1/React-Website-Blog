a58575953673efcf2ab7877611b4f8e6d7a4cc3e
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2023-04-05 01:03:03 +0200

    new

--- src/components/404.jsx
+++ src/components/404.jsx
@@ -2,10 +2,8 @@
 
 import React from 'react'
 import { Link } from 'react-router-dom'
-import {useFetch} from '../hooks/useFetch'
 
 export default function Page404() {
-  const data = useFetch('https://jsonplaceholder.typicode.com/posts/1');
   return (
     // create me a 404 page
     <div className='Page404'>
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,6 +1,3 @@
-
-
-
 import React from 'react'
 import '../card.css'
 import {Link} from 'react-router-dom'
